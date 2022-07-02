89099f7ebad3e4849694d033fb70970f208971e8
istrav <travis.burandt@gmail.com>
2022-07-02 12:26:39 -0500

    platforms 3

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // import { WsAdapter } from '@nestjs/platform-ws';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
 
 let PORT = process.env.PORT || 8888
 
@@ -10,8 +12,14 @@ async function bootstrap() {
   app.enableCors()
   // app.useWebSocketAdapter(new WsAdapter(app));
 
+  // Proxy endpoints
+  app.use('/', createProxyMiddleware({
+    target:  "https://istrav-headquarters.vercel.app",
+    changeOrigin: true,
+  }));
+
+  // attach
   await app.listen(PORT);
-  
   console.log(`Application is running on ${await app.getUrl()}`)
 }
 bootstrap();
